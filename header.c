a6c1d287cdba55b8912a0e81392d1049b93a055a
Mariomm505 <ems08510@gmail.com>
2026-04-24 12:22:15 +0300

    сортировка, замена строчки

--- header.c
+++ header.c
@@ -15,13 +15,17 @@ void my_sort(struct result results[], int count) {
 	}
 }
 
-void my_fprint(struct result results[], int count){
-	FILE *out = fopen("out.txt", "w");	
+
+void my_fprint(struct result results[], int count)
+{
+	FILE* out = fopen("out.txt", "w");
 	fprintf(out, "Название груза\t\tКоличество стеллажей\n");
 	for (int i = 0; i < count; i++) {
-		fprintf(out, "%s\t\t%d\n", results[i].name, results[i].polka_count);}
+		fprintf(out, "%s\t\t%d\n", results[i].name, results[i].polka_count);
+	}
 	fclose(out);
 }
+//погоди даня а где обработка ошибки открытия файла??
 
 void process(struct tovar ar[], int count) {
 	struct result results[100];
@@ -38,7 +42,8 @@ void process(struct tovar ar[], int count) {
 			strcpy(results[result_count].name, ar[i].name);
 			results[result_count].polka_count = 1;
 			result_count++;
-		} else {
+		}
+		else {
 			int shelf_exists = 0;
 			for (int k = 0; k < i; k++) {
 				if (strcmp(ar[k].name, ar[i].name) == 0 && ar[k].polka == ar[i].polka) {
@@ -51,78 +56,128 @@ void process(struct tovar ar[], int count) {
 			}
 		}
 	}
-	
+
 	my_sort(results, result_count);
 	my_fprint(results, result_count);
-}
 
-int menu(){
-	int number;
-	printf("Меню:\n0. Выход\n1. Загрузить данные из входного файла в массив структур\n2. Добавить строчку в массив\n3. Удалить строчку по ключу\n4. Заменить строчку\n5. Сортировка вставкой\n6. Сортировка выбором\n7. Сортировка обменом (пузырек)\n8. Сохранить данные в файл\n9. Отпечатать файл или массив с разбивкой по страницам и шапкой с номером страницы\n");
-	scanf("%d", &number);
-	switch (number) {
-	case 0:
-		//0. Выход
-		break;
-	case 1:
-		//1. Загрузить данные из входного файла в массив структур
-		break;
-	case 2:
-		//2. Добавить строчку в массив
-		break;
-	case 3:
-		//3. Удалить строчку по ключу
-		break;
-	case 4:
-		//4. Заменить строчку
-		break;
-	case 5:
-		//5. Сортировка вставкой
-		break;
-	case 6:
-		//6. Сортировка выбором
-		break;
-	case 7:
-		//7. Сортировка обменом (пузырек)
-		break;
-	case 8:
-		//8. Сохранить данные в файл
-		break;
-	case 9:
-		//9. Отпечатать файл или массив с разбивкой по страницам и шапкой с номером страницы
-		break;
-	default:
-		printf("Введите число от 0 до 9\n");
-		break;
+	//от мариооо (пункт 9)
+	//меням функцию чтоб выводила по страничкам
+	//я предполагаю что должно выглядеть так:
+	//Страница 1
+	//Название груза    Количество стеллажиков
+	//=========================================
+	//Фрукты всякие, продукты      количество стеллажей
+
+	//а ля я туплю надо переделать, че я вообще написала
+	void print_file_pagik_trushniy(struct result results[], int count) {
+		FILE* out = fopen("out.txt", "w");
+
+		//даня вот обработчик ошибки, но мб я в глаза долблюсь у тебя не увидела потому что упоролась
+		if (!out) {
+			printf("Ошибка открытия файла\n");
+			return;
+		}
+		int page_size = 3;  //строки на странице
+		int line = 0;
+		int page = 1;
+
+		for (int i = 0; i < count; i++) {
+
+			// новая страница
+			if (line % page_size == 0) {
+				fprintf(out, "\nСтраница %d\n", page++);
+				fprintf(out, "Название груза\t\tКоличество стеллажей\n");
+				fprintf(out, "========================================\n");
+			}
+
+			fprintf(out, "%-20s %5d\n",
+				results[i].name,
+				results[i].polka_count); //-20 эт выравнивание текста шоб по красоте
+			line++;
+		}
+
+		fclose(out);
 	}
-}
 
-//сортировка выбором от мариоооо (6 пункт)
-void sort_choice(struct result results[], int count) {
-	for (int i = 0; i < count - 1; i++) {
-		int min_ind = i;
-		for (int j = i + 1; j < count; j++) {
-			if (strcmp(results[j].name, results[min_ind].name) < 0) {
-				min_ind = j;
+	//сортировка выбором от мариоооо (6 пункт)
+	void sort_choice(struct result results[], int count) {
+		for (int i = 0; i < count - 1; i++) {
+			int min_ind = i;
+			for (int j = i + 1; j < count; j++) {
+				if (strcmp(results[j].name, results[min_ind].name) < 0) {
+					min_ind = j;
+				}
+			}
+			if (min_ind != i) {
+				struct result temp = results[i];
+				results[i] = results[min_ind];
+				results[min_ind] = temp;
 			}
 		}
-		if (min_ind != i) {
-			struct result temp = results[i];
-			results[i] = results[min_ind];
-			results[min_ind] = temp;
+	}
+	//сортировка вставкой от мариоооо (5 пункт)
+	void sort_input(struct result results[], int count) {
+		for (int i = 1; i < count; i++) {
+			struct result key = results[i];
+			int j = i - 1;
+
+			while (j >= 0 && strcmp(results[j].name, key.name) > 0) {
+				results[j + 1] = results[j];
+				j--;
+			}
+			results[j + 1] = key;
 		}
 	}
-}
-//сортировка вставкой от мариоооо (5 пункт)
-void sort_input(struct result results[], int count) {
-	for (int i = 1; i < count; i++) {
-		struct result key = results[i];
-		int j = i - 1;
-		
-		while (j >= 0 && strcmp(results[j].name, key.name) > 0) {
-			results[j + 1] = results[j];
-			j--;
+
+	//пункт 4. Замена строчки
+	//если речь о перезаписи в строчке то тут проблемка, ибо в C нельзя перезаписать строчку если длинна отличается
+	//я попробую накидать, но по сути чтоб перезаписать надо тупо файл пересоздавать
+
+	void replacee() {
+		FILE* in = fopen("in.txt", "r");
+		FILE* out = fopen("out.txt", "w");
+		//дальше обработочка ошибочки
+		if (!in || !out) {
+			printf("Ошибка открытия файла\n");
+			if (in) fclose(in);
+			if (out) fclose(out);
+			return;
 		}
-		results[j + 1] = key;
+		char line[256];
+		char new_namik[100];
+		int new_count;
+		int chosen_line;
+		int now_line = 0;
+
+		printf("Введите номер строки, которую хотите заменить: ");
+		scanf("%d", &chosen_line);
+		printf("Введите новое название груза: ");
+		scanf("%99s", new_namik);
+		printf("Введите новое количество стеллажей: ");
+		scanf("%d", &new_count);
+
+
+		while (fgets(line, sizeof(line), in)) {
+			if (now_line < 2) {
+				fputs(line, out);
+			}
+			else {
+				int data_ind = now_line - 2;
+				if (data_ind == chosen_line - 1) {
+					fprintf(out, "%-20s %5d\n", new_namik, new_count);
+				}
+				else {
+					fputs(line, out);
+				}
+			}
+			now_line++;
+		}
+		fclose(in);
+		fclose(out);
+		remove("in.txt");
+		rename("out.txt", "in.txt");
 	}
-}
+
+	//Пункт 3. удаление строчки по ключууу
+	//
+
